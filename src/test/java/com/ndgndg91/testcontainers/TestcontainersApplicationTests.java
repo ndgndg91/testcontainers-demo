f3b71a430ffa39e32425c04ec6863178980c02b3
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 <ndgndg91@gmail.com>
2021-12-20 22:06:04 +0900

    init project

---
+++ src/test/java/com/ndgndg91/testcontainers/TestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndgndg91.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
